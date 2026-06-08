5ed5108c6763de08c51de34d08a6287a342fb859
Paulo Schiochet <paulsix02@gmail.com>
2026-06-08 03:46:52 -0300

    feat: adiciona boilerplate basico do react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Currículo Online - GRR20242749</h1>
+      <p>Este é um portfólio placeholder básico sem estilos.</p>
+      <p>Em breve, o layout clássico de computador será inserido aqui.</p>
+    </div>
+  );
+}
+
+export default App;
